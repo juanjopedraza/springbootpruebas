a580675ac1141df3a8d1a5e1c7e22e26910a9ab3
Juanjo <juanjo@fp.edu.es>
2022-11-22 16:39:14 +0100

    Clase 22/11/2022

--- src/main/java/com/example/demo/controlador/EmpleadoController.java
+++ src/main/java/com/example/demo/controlador/EmpleadoController.java
@@ -1,11 +1,14 @@
 package com.example.demo.controlador;
 
 import com.example.demo.Dominio;
+import com.example.demo.modelo.Empleado;
 import com.example.demo.servicios.EmpleadoServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.net.MalformedURLException;
 
@@ -14,7 +17,6 @@ public class EmpleadoController {
 
     @Autowired
     private EmpleadoServicio servicio;
-
 
     @GetMapping({"empleado/list"})
     public String listado (Model model) {
@@ -25,6 +27,23 @@ public class EmpleadoController {
         model.addAttribute("pagina","list");
         model.addAttribute("listaEmpleados",servicio.findAll());
         return "index";
+    }
+    @GetMapping({"empleado/alta"})
+    public String alta (Model model) {
+
+        model.addAttribute("mensaje","Ramoncín esto es una prueba");
+        model.addAttribute("dominio","dom.getDominio()");
+        model.addAttribute("tld","dom.getTld()");
+        model.addAttribute("empleadoForm", new Empleado());
+        model.addAttribute("pagina","alta");
+
+        return "index";
+    }
+
+    @PostMapping("/empleado/new/submit")
+    public String nuevoEmpleadoSubmit(@ModelAttribute("empleadoForm") Empleado nuevoEmpleado) {
+        servicio.add(nuevoEmpleado);
+        return "redirect:/empleado/list";
     }
 
 }
